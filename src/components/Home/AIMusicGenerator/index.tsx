0db8b23b4f37b88b10274d56df37db1780c93ef2
bidishasengupta2025 <basudebsg2022@gmail.com>
2025-04-30 20:33:50 -0400

    Upgrade now

--- src/components/Home/AIMusicGenerator/index.tsx
+++ src/components/Home/AIMusicGenerator/index.tsx
@@ -309,8 +309,16 @@ const AIMusicGenerator = () => {
             </div>
           )}
 
-            <div className="text-xs text-white/90 mt-1">
-            ✨ Upgrade to download tracks
+            <div 
+              className="text-xs text-white/90 mt-1 cursor-pointer hover:text-[#f15107] transition-colors"
+              onClick={() => {
+                document.getElementById('pricing')?.scrollIntoView({ 
+                  behavior: 'smooth',
+                  block: 'start'
+                });
+              }}
+            >
+              ✨ Upgrade to download tracks
             </div>
           </div>
         </div>
