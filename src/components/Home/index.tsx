16910248489b6348472614e380c6812ff31b3e04
bidishasengupta2025 <basudebsg2022@gmail.com>
2025-06-15 01:16:14 -0400

    remove preview

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -5,14 +5,12 @@ import Counter from "./Counter";
 import FAQ from "./FAQ";
 import Testimonials from "./Testimonials";
 import Pricing from "./Pricing";
-import Preview from "./Preview";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Features />
-      <Preview />
       <FeaturesWithImage />
       <Counter />
       <Testimonials />
